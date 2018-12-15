26eca2074de4cc754689b877c67fa60dafff05f2
swapnil <swapnil190296@gmail.com>
2018-12-14 20:48:47 -0500

    research project

--- examples/wifi/smart_config/main/smartconfig_main.c
+++ examples/wifi/smart_config/main/smartconfig_main.c
@@ -20,7 +20,12 @@
 #include "nvs_flash.h"
 #include "tcpip_adapter.h"
 #include "esp_smartconfig.h"
+#include "mbedtls/pk.h"
 
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include <lwip/netdb.h>
 /* FreeRTOS event group to signal when we are connected & ready to make a request */
 static EventGroupHandle_t s_wifi_event_group;
 
@@ -31,6 +36,12 @@ static const int CONNECTED_BIT = BIT0;
 static const int ESPTOUCH_DONE_BIT = BIT1;
 static const char *TAG = "sc";
 
+
+#define MESSAGE "Hello TCP Client!!"
+#define LISTENQ 2
+
+static EventGroupHandle_t wifi_event_group;
+ 
 void smartconfig_example_task(void * parm);
 
 static esp_err_t event_handler(void *ctx, system_event_t *event)
@@ -100,6 +111,75 @@ static void sc_callback(smartconfig_status_t status, void *pdata)
     }
 }
 
+void tcp_server(void *pvParam){
+    ESP_LOGI(TAG,"tcp_server task started \n");
+    struct sockaddr_in tcpServerAddr;
+    tcpServerAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    tcpServerAddr.sin_family = AF_INET;
+    tcpServerAddr.sin_port = htons( 4444 );
+    int s, r;
+    char recv_buf[64];
+    static struct sockaddr_in remote_addr;
+    static unsigned int socklen;
+    socklen = sizeof(remote_addr);
+    int cs;//client socket
+    xEventGroupWaitBits(wifi_event_group,CONNECTED_BIT,false,true,portMAX_DELAY);
+        ESP_LOGI(TAG,"tcp_server task started1 \n");
+
+    while(1){
+        s = socket(AF_INET, SOCK_STREAM, 0);
+        if(s < 0) {
+            ESP_LOGE(TAG, "... Failed to allocate socket.\n");
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            continue;
+        }
+        ESP_LOGI(TAG, "... allocated socket\n");
+         if(bind(s, (struct sockaddr *)&tcpServerAddr, sizeof(tcpServerAddr)) != 0) {
+            ESP_LOGE(TAG, "... socket bind failed errno=%d \n", errno);
+            close(s);
+            vTaskDelay(4000 / portTICK_PERIOD_MS);
+            continue;
+        }
+        ESP_LOGI(TAG, "... socket bind done \n");
+        if(listen (s, LISTENQ) != 0) {
+            ESP_LOGE(TAG, "... socket listen failed errno=%d \n", errno);
+            close(s);
+            vTaskDelay(4000 / portTICK_PERIOD_MS);
+            continue;
+        }
+        while(1){
+            cs=accept(s,(struct sockaddr *)&remote_addr, &socklen);
+            ESP_LOGI(TAG,"New connection request,Request data:");
+            //set O_NONBLOCK so that recv will return, otherwise we need to impliment message end 
+            //detection logic. If know the client message format you should instead impliment logic
+            //detect the end of message
+            fcntl(cs,F_SETFL,O_NONBLOCK);
+            do {
+                bzero(recv_buf, sizeof(recv_buf));
+                r = recv(cs, recv_buf, sizeof(recv_buf)-1,0);
+                for(int i = 0; i < r; i++) {
+                    putchar(recv_buf[i]);
+                }
+            } while(r > 0);
+            
+            ESP_LOGI(TAG, "... done reading from socket. Last read return=%d errno=%d\r\n", r, errno);
+            
+            if( write(cs , MESSAGE , strlen(MESSAGE)) < 0)
+            {
+                ESP_LOGE(TAG, "... Send failed \n");
+                close(s);
+                vTaskDelay(4000 / portTICK_PERIOD_MS);
+                continue;
+            }
+            ESP_LOGI(TAG, "... socket send success");
+            close(cs);
+        }
+        ESP_LOGI(TAG, "... server will be opened in 5 seconds");
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+    }
+    ESP_LOGI(TAG, "...tcp_client task closed\n");
+}
+
 void smartconfig_example_task(void * parm)
 {
     EventBits_t uxBits;
@@ -116,11 +196,119 @@ void smartconfig_example_task(void * parm)
             vTaskDelete(NULL);
         }
     }
+    rsabasedcomm();
+
+}
+
+
+/*
+ * Example RSA-1024 keypair, for test purposes
+ */
+#define KEY_LEN 256
+//#define KEY_LEN 199
+
+#define RSA_N   "9292758453063D803DD603D5E777D788" \
+                "8ED1D5BF35786190FA2F23EBC0848AEA" \
+                "DDA92CA6C3D80B32C4D109BE0F36D6AE" \
+                "7130B9CED7ACDF54CFC7555AC14EEBAB" \
+                "93A89813FBF3C4F8066D2D800F7C38A8" \
+                "1AE31942917403FF4946B0A83D3D3E05" \
+                "EE57C6F5F5606FB5D4BC6CD34EE0801A" \
+                "5E94BB77B07507233A0BC7BAC8F90F79"
+#define RSA_E   "10001"
+
+#define RSA_D   "24BF6185468786FDD303083D25E64EFC" \
+                "66CA472BC44D253102F8B4A9D3BFA750" \
+                "91386C0077937FE33FA3252D28855837" \
+                "AE1B484A8A9A45F7EE8C0C634F99E8CD" \
+                "DF79C5CE07EE72C7F123142198164234" \
+                "CABB724CF78B8173B9F880FC86322407" \
+                "AF1FEDFDDE2BEB674CA15F3E81A1521E" \
+                "071513A1E85B5DFA031F21ECAE91A34D"
+
+#define RSA_P   "C36D0EB7FCD285223CFB5AABA5BDA3D8" \
+                "2C01CAD19EA484A87EA4377637E75500" \
+                "FCB2005C5C7DD6EC4AC023CDA285D796" \
+                "C3D9E75E1EFC42488BB4F1D13AC30A57"
+
+#define RSA_Q   "C000DF51A7C77AE8D7C7370C1FF55B69" \
+                "E211C2B9E5DB1ED0BF61D0D9899620F4" \
+                "910E4168387E3C30AA1E00C339A79508" \
+                "8452DD96A9A5EA5D9DCA68DA636032AF"
+
+#define PT_LEN  10
+
+
+#if defined(MBEDTLS_PKCS1_V15)
+static int myrand( void *rng_state, unsigned char *output, size_t len )
+{
+#if !defined(__OpenBSD__)
+    size_t i;
+
+    if( rng_state != NULL )
+        rng_state  = NULL;
+
+    for( i = 0; i < len; ++i )
+        output[i] = rand();
+#else
+    if( rng_state != NULL )
+        rng_state = NULL;
+
+    arc4random_buf( output, len );
+#endif /* !OpenBSD */
+
+    return( 0 );
+}
+#endif /* MBEDTLS_PKCS1_V15 */
+
+void rsabasedcomm() 
+{
+//#if defined(MBEDTLS_PKCS1_V15)
+    size_t len;
+    mbedtls_rsa_context rsa;
+    unsigned char rsa_plaintext[PT_LEN];
+    unsigned char rsa_decrypted[PT_LEN];
+    //unsigned char rsa_ciphertext[] = "859c4e7df7eb356915b3d615ace10dd283c607f13166add1c716ef5c22bf7fddf7bded77ecfb2de0aabc7ecc3fbbbe5f84455487a62a58264f0bff728f7e27170d65cac2122497e7133eae7186cc9f124638219d404f6bd3d48361ea3775d54827f241dd0ecc1b3feb40e28d41e9054fb37809938813978ee8a14f242bd55268";
+    mbedtls_mpi K;
+
+    mbedtls_mpi_init( &K );
+    mbedtls_rsa_init( &rsa, MBEDTLS_RSA_PKCS_V15, 0 );
+
+// Creating the context for RSA
+    mbedtls_mpi_read_string( &K, 16, RSA_N  );
+    mbedtls_rsa_import( &rsa, &K, NULL, NULL, NULL, NULL );
+    mbedtls_mpi_read_string( &K, 16, RSA_P  );
+    mbedtls_rsa_import( &rsa, NULL, &K, NULL, NULL, NULL );
+    mbedtls_mpi_read_string( &K, 16, RSA_Q  );
+    mbedtls_rsa_import( &rsa, NULL, NULL, &K, NULL, NULL );
+    mbedtls_mpi_read_string( &K, 16, RSA_D  );
+    mbedtls_rsa_import( &rsa, NULL, NULL, NULL, &K, NULL );
+    mbedtls_mpi_read_string( &K, 16, RSA_E );
+    mbedtls_rsa_import( &rsa, NULL, NULL, NULL, NULL, &K );
+
+    mbedtls_rsa_complete( &rsa );
+
+    TAG = "encrypted:";
+//Print encrypted text
+    ESP_LOGI(TAG, "%s", rsa_ciphertext);
+//Decryption 
+    if( mbedtls_rsa_pkcs1_decrypt( &rsa, myrand, NULL, MBEDTLS_RSA_PRIVATE,
+                                   &len, rsa_ciphertext, rsa_decrypted,
+                                   sizeof(rsa_decrypted) ) != 0 )
+    {
+    }
+    TAG = "decrypted:";
+
+    ESP_LOGI(TAG, "%s", rsa_decrypted);
+
+
 }
 
 void app_main()
 {
     ESP_ERROR_CHECK( nvs_flash_init() );
     initialise_wifi();
+    xTaskCreate(&tcp_server,"tcp_server",4096,NULL,5,NULL);
 }
+
 
